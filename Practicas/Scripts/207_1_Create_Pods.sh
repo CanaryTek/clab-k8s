670ecc7f75a0d2b2cea979eb80202fc5c5364170
Kuko Armas <kuko@canarytek.com>
2020-02-05 10:14:31 +0000

    fix permissions bug

--- Practicas/Scripts/207_1_Create_Pods.sh
+++ Practicas/Scripts/207_1_Create_Pods.sh
@@ -13,7 +13,7 @@ spec:
 
 ssh node01 "sudo mkdir /etc/para_complicarlo >/dev/null 2>&1"
 ssh node01 "test -f /var/lib/kubelet/config.yaml.bck || sudo cp /var/lib/kubelet/config.yaml{,.bck}"
-ssh node01 "sed 's/kubernetes\/manifests/para_complicarlo/' < /var/lib/kubelet/config.yaml.bck | sudo tee /var/lib/kubelet/config.yaml >/dev/null && sudo systemctl restart kubelet"
+ssh node01 "sudo cat /var/lib/kubelet/config.yaml.bck | sed 's/kubernetes\/manifests/para_complicarlo/' | sudo tee /var/lib/kubelet/config.yaml >/dev/null && sudo systemctl restart kubelet"
 
 echo "$YAML" | ssh node01 "cat - | sudo tee /etc/para_complicarlo/pod.yaml >/dev/null"
 
